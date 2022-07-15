dd8893bd9c5a3dcca9c90ff1c77aeb93e942f0df
sathya <sathya.srinivasan@gmail.com>
2022-07-15 14:38:36 +1000

    dem

---
+++ class_eg.py
@@ -0,0 +1,19 @@
+class Worker:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def lastName (self):
+        return self.name.split()[-1]
+
+    def grow (self, how_much):
+        self.age = self.age + how_much
+
+
+
+sat = Worker('sathya sri',40)
+bat = Worker('kaka jojo',30 )
+
+sat.lastName()
+sat.name
+bat.grow(2)
+bat.age
